096c6fb19a4edad4910eb37ac30d814f29479e70
RahulSingh044 <rahulsingh.dev.36@gmail.com>
2026-02-10 06:29:15 +0530

    fix: add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,10 @@ dotenv.config();
 const app = express();
 
 //middleware
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
